77c854bbefe939a369b8410b1d6b13df33d60cc5
plowsof <77655812+plowsof@users.noreply.github.com>
2022-05-17 01:03:55 +0100

    Update index_alphabetic.py

--- helper-scripts/index_alphabetic.py
+++ helper-scripts/index_alphabetic.py
@@ -8,31 +8,46 @@ en_yaml = "./monero-site/_i18n/en.yml"
 with open(en_yaml, 'r') as stream:
     data_loaded = yaml.safe_load(stream)
 
-with open("testing.md", "r") as f:
-	soup = BeautifulSoup(f, "html.parser")
+with open("./monero-site/resources/user-guides/index.md", "r") as f:
+    soup = BeautifulSoup(f, "html.parser")
 
 divs = soup.find_all("div", {"class": "col"})
 data = {}
 for div in divs:
-	category = str(div.find("h2"))
-	category = category.split(".")[1].split(" ")[0]
-	data[category] = []
-	p = div.find_all("p")
-	for lol in p:
-		#link = lol.find_all("a",href=True)[0]
-		#print(link['href'])
-		title_key = lol.text.split(".")[1].split(" ")[0]
-		title_yaml = data_loaded["user-guides"][title_key]
-		info = { 
-					"title_key": title_key,
-					"title_yaml": title_yaml,
-					"raw_data": lol
-				}
-		data[category].append(info)
-		
-for category in data:
-	alphabetic = sorted(data[category], key=lambda d: d['title_yaml']) 
-	data[category] = alphabetic
+    category = str(div.find("h2"))
+    category = category.split(".")[1].split(" ")[0]
+    data[category] = []
+    p = div.find_all("p")
+    for lol in p:
+        #link = lol.find_all("a",href=True)[0]
+        #print(link['href'])
+        title_key = lol.text.split(".")[1].split(" ")[0]
+        title_yaml = data_loaded["user-guides"][title_key]
+        info = { 
+                    "title_key": title_key,
+                    "title_yaml": title_yaml,
+                    "raw_data": lol
+                }
+        data[category].append(info)
 
-pprint.pprint(data)
-pprint.pprint(data)
+for category in data:
+    alphabetic = sorted(data[category], key=lambda d: d['title_yaml']) 
+    data[category] = alphabetic
+
+with open("index_template.md", "r") as f:
+    lines = f.readlines()
+
+with open("index.md", "w+") as f:
+    for line in lines:
+        if "!_!" not in line:
+            f.write(line)
+        else:
+            category = line.split("!_!")[1].strip()
+            for thing in data[category]:
+                raw_data = thing["raw_data"]
+                indent = ""
+                for i in range(28):
+                    indent += " "
+
+                f.write(f"{indent}{thing['raw_data']}\n")
+
