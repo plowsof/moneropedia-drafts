62b41a72bfee125716c15ebc148b2e8c8f34962b
plowsof <77655812+plowsof@users.noreply.github.com>
2022-06-01 00:29:42 +0100

    file = 1 outside of loop

--- .github/workflows/alphabetic-userguide.py
+++ .github/workflows/alphabetic-userguide.py
@@ -26,19 +26,17 @@ for div in divs:
                     "raw_data": lol
                 }
         data[category].append(info)
-try:
-    for category in data:
-        original = data[category]
-        alphabetic = sorted(data[category], key=lambda d: d['title_yaml']) 
-        data[category] = alphabetic
-        fine = 1
-        if original == alphabetic:
-            print(f"--> Category: {category} [ OK ]")
-        else:
-            print(f"--> Category: {category} [ FAIL ]")
-            fine = 0
-except:
-    print("huh?")
+
+fine = 1
+for category in data:
+    original = data[category]
+    alphabetic = sorted(data[category], key=lambda d: d['title_yaml']) 
+    data[category] = alphabetic
+    if original == alphabetic:
+        print(f"--> Category: {category} [ OK ]")
+    else:
+        print(f"--> Category: {category} [ FAIL ]")
+        fine = 0
 
 if fine == 0:
     sys.exit(1)
